d629baa8794ed16c73de73cb58d214d5bbfe91bc
jasper <grimmsper@gmail.com>
2016-01-11 14:22:29 +0300

    fix

--- bin/modvert.web.php
+++ bin/modvert.web.php
@@ -6,8 +6,25 @@
  * Time: 2:05 PM
  */
 
-require __DIR__ . '/../vendor/autoload.php';
-
+$root = __DIR__.'/../';
+$path = str_replace('/', DIRECTORY_SEPARATOR, $root.'vendor/autoload.php');
+if (file_exists($path)) {
+   include_once $path;
+} else{
+   $path = str_replace('/', DIRECTORY_SEPARATOR, $root.'../vendor/autoload.php');
+   if (file_exists($path)) {
+       include_once $path;
+   } else {
+       $path = str_replace('/', DIRECTORY_SEPARATOR, $root.'../../autoload.php');
+       if (file_exists($path)) {
+           include_once $path;
+       } else {
+           echo 'Something goes wrong with your archive'.PHP_EOL.
+               'Try downloading again'.PHP_EOL;
+           exit(1);
+       }
+   }
+}
 define('TARGET_PATH', realpath(getcwd() . DIRECTORY_SEPARATOR . '..'));
 
 /** @var \Modvert\Application $app */
