a782e46095019c833ee1d12aa87a8f463d55de18
vestnik <grimmsper@gmail.com>
2016-01-24 06:34:08 +0300

    fix

--- src/Modvert/Serializer/PHPSerializer.php
+++ src/Modvert/Serializer/PHPSerializer.php
@@ -40,15 +40,20 @@ class PHPSerializer extends Serializer
             if (T_DOC_COMMENT == $token[0]) {
                 $docblock = $token[1];
                 $content = str_replace($docblock, '', $source);
-                $content = preg_replace("/\r\n/", "\n", $content);
+                $content = preg_replace("/\\r\\n/", "\n", $content);
                 $content = preg_replace('/\<\?php[\r\n]?(.*)/sm', '${1}', $content);
                 $content = preg_replace('/^\s+$/s', "\n", $content);
-                $content = preg_replace('/^\n$/m', '', $content, 1);
+                $content = preg_replace('/^(\r*\n)$/m', '', $content, 1);
                 break;
             }
         }
         $docblock = preg_replace('/\/\*\*(.*)\*\//s', '${1}', $docblock);
         $data = eval($docblock);
+        if ($content) {
+            if ($content[0] == "\n" || $content[0] == "\r\n") {
+                $content = preg_replace("/^\n/", "", $content, 1);
+            }
+        }
         $data['content'] = $content;
         return $data;
     }
